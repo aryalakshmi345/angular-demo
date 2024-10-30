43bfd45166274f311f5d1f9ec9ce82b126cad602
aryalakshmi345 <lekshmiarya113@gmail.com>
2024-10-30 10:33:25 +0530

    second commit

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -8,12 +8,16 @@ import { Component } from '@angular/core';
 export class EmployeeComponent {
    empName:string = "Max Miller"
    empURL:string = "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Free-Download.png"
+   empDesig:string = "Dummy"
+   empSalary:string = ""
 
    userAdd = ()=>{
     alert("Add button clicked..!")
    }
 
    getEmpName(event:any){
+    console.log(event.target.value);
+    
     this.empName = event.target.value
    }
 
@@ -25,5 +29,13 @@ export class EmployeeComponent {
       alert('Please Fill the form...')
     }
     
+   }
+
+   empSubmit(){
+    if(!this.empName || !this.empDesig || !this.empSalary){
+      alert("Please fill the form")
+    }else{
+      alert(`Name: ${this.empName}, Designation: ${this.empDesig}`)
+    }
    }
 }
